858eea3c0359cddd69994e64a9705e2589c7d1e2
Ralph Christopher Castillo <rapi.castillo@gmail.com>
2015-07-05 12:36:07 -0400

    Add Google Verification

---
+++ inc/_head.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+
+<meta charset="utf-8" />
+<meta name="google-site-verification" content="<meta name="google-site-verification" content="-jE-f4Gbpim9_feo74iK5zP_-tegU7xvV89-yqFy7ZI" />" />
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="description" content="All events related to Bernie. Townhall meetings, meetups, etc. Click on state to filter results.">
+<meta name="keywords" content="Bernie Sanders, FeelTheBern, Events, Bernie, #bernie2016, #feelthebern">
+<meta property="og:image" content="http://www.bernie2016events.org/img/bernie-logo.png"/>
+<meta property="og:title" content="Bernie 2016 - Events Around the States"/>
+<meta property="og:description" content="All events related to Bernie. Townhall meetings, meetups, etc. Click on state to filter results."/>
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700,800">
+<title>#Bernie2016 Events Around the States - All events related to Bernie Sanders. Townhall meetings, meetups, etc. Click on state to filter results.</title>
+<link rel="shortcut icon" href="http://www.bernie2016events.org/favicon.ico">
+</head>
