924b59f7ccf40e47cf886c86aa1d40b9289a6661
Ralph Christopher Castillo <rapi.castillo@gmail.com>
2015-07-22 19:39:11 -0400

    X

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+
+  $mc = new Memcached();
+
+  if ($mc) {
+
+    $mc->setOption(Memcached::OPT_BINARY_PROTOCOL, true);
+    $mc->addServers(array_map(function($server) { return explode(':', $server, 2); }, explode(',', $_ENV['MEMCACHEDCLOUD_SERVERS'])));
+    $mc->setSaslAuthData($_ENV['MEMCACHEDCLOUD_USERNAME'], $_ENV['MEMCACHEDCLOUD_PASSWORD']);
+
+    $mc->set('foo', 'bar');
+    echo $mc->get('foo');
+
+  }
+?>
