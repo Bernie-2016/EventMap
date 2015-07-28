43db350a3366be72d7992667ee993a53d3640708
bahoo <jon.c.culver@gmail.com>
2015-07-28 00:30:40 -0400

    WIP on automatic data updates.

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,11 @@
 import json
+import random
 import requests
+import string
 
 from fabric.api import *
 from fabric.operations import local
+from string import Template
 
 def update_event_data():
     
@@ -66,9 +69,97 @@ def update_event_data():
 
 
 def deploy_event_data():
-    update_event_data()
-    local("aws s3 cp js/bern-map-async.gz s3://map.berniesanders.com/js/bern-map-async.gz --metadata-directive REPLACE --content-encoding \"gzip\" --content-type \"text/javascript\" --region \"us-west-2\"")
-    # todo: clear cache invalidation
+    # update_event_data()
+    # local("aws s3 cp js/bern-map-async.gz s3://map.berniesanders.com/js/bern-map-async.gz --metadata-directive REPLACE --content-encoding \"gzip\" --content-type \"text/javascript\" --region \"us-west-2\"")
+    filein = open('cache-invalidation-tpl.xml')
+    src = Template( filein.read() )
+    invalidation_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(12))
+    print src.substitute({'invalidation_id': invalidation_id})
+
+    def sign(key, msg):
+        return hmac.new(key, msg.encode("utf-8"), hashlib.sha256).digest()
+
+    def getSignatureKey(key, date_stamp, regionName, serviceName):
+        kDate = sign(('AWS4' + key).encode('utf-8'), date_stamp)
+        kRegion = sign(kDate, regionName)
+        kService = sign(kRegion, serviceName)
+        kSigning = sign(kService, 'aws4_request')
+        return kSigning
+
+    # Read AWS access key from env. variables or configuration file. Best practice is NOT
+    # to embed credentials in code.
+    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    if access_key is None or secret_key is None:
+        print 'No access key is available.'
+        sys.exit()
+
+    # Create a date for headers and the credential string
+    t = datetime.datetime.utcnow()
+    amz_date = t.strftime('%Y%m%dT%H%M%SZ')
+    date_stamp = t.strftime('%Y%m%d') # Date w/o time, used in credential scope
+
+
+    # ************* TASK 1: CREATE A CANONICAL REQUEST *************
+    # http://docs.aws.amazon.com/general/latest/gr/sigv4-create-canonical-request.html
+
+    # Step 1 is to define the verb (GET, POST, etc.)--already done.
+
+    # Step 2: Create canonical URI--the part of the URI from domain to query 
+    # string (use '/' if no path)
+    canonical_uri = '/'
+
+    ## Step 3: Create the canonical query string. In this example, request
+    # parameters are passed in the body of the request and the query string
+    # is blank.
+    canonical_querystring = ''
+
+    # Step 4: Create the canonical headers. Header names and values
+    # must be trimmed and lowercase, and sorted in ASCII order.
+    # Note that there is a trailing \n.
+    canonical_headers = 'content-type:' + content_type + '\n' + 'host:' + host + '\n' + 'x-amz-date:' + amz_date + '\n' + 'x-amz-target:' + amz_target + '\n'
+
+    # Step 5: Create the list of signed headers. This lists the headers
+    # in the canonical_headers list, delimited with ";" and in alpha order.
+    # Note: The request can include any headers; canonical_headers and
+    # signed_headers include those that you want to be included in the
+    # hash of the request. "Host" and "x-amz-date" are always required.
+    # For DynamoDB, content-type and x-amz-target are also required.
+    signed_headers = 'content-type;host;x-amz-date;x-amz-target'
+
+    # Step 6: Create payload hash. In this example, the payload (body of
+    # the request) contains the request parameters.
+    payload_hash = hashlib.sha256(request_parameters).hexdigest()
+
+    # Step 7: Combine elements to create create canonical request
+    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+
+
+    # ************* TASK 2: CREATE THE STRING TO SIGN*************
+    # Match the algorithm to the hashing algorithm you use, either SHA-1 or
+    # SHA-256 (recommended)
+    algorithm = 'AWS4-HMAC-SHA256'
+    credential_scope = date_stamp + '/' + region + '/' + service + '/' + 'aws4_request'
+    string_to_sign = algorithm + '\n' +  amz_date + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request).hexdigest()
+
+
+    # ************* TASK 3: CALCULATE THE SIGNATURE *************
+    # Create the signing key using the function defined above.
+    signing_key = getSignatureKey(secret_key, date_stamp, region, service)
+
+    # Sign the string_to_sign using the signing_key
+    signature = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+
+
+    # ************* TASK 4: ADD SIGNING INFORMATION TO THE REQUEST *************
+    # Put the signature information in a header named Authorization.
+    authorization_header = algorithm + ' ' + 'Credential=' + access_key + '/' + credential_scope + ', ' +  'SignedHeaders=' + signed_headers + ', ' + 'Signature=' + signature
+
+
+
+
+    
+    local('curl -H "Content-Type: text/xml" -H "Authorization: %s" http://cloudfront.amazonaws.com/distribution/E2SIHVDQUPIR5Z/invalidation')
 
 
 
