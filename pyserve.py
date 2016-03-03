c3abd98c2a215fc54d8a26b0bddadf95d86ac40d
bahoo <jon.c.culver@gmail.com>
2016-03-03 13:27:49 -0800

    Way saner local hosting.

---
+++ pyserve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import SimpleHTTPServer
+
+class GZipFriendlyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+
+        SimpleHTTPServer.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        MONKEYPATCHED_HEADERS = {
+            '/d/us_postal_codes.gz': {
+                'Content-Encoding': 'gzip',
+                'Content-Type': 'text/csv'
+            },
+            '/js/event-data.gz': {
+                'Content-Encoding': 'gzip',
+                'Content-Type': 'text/javascript'  
+            }
+        }
+
+        if self.path in MONKEYPATCHED_HEADERS:
+            for header, value in MONKEYPATCHED_HEADERS[self.path].iteritems():
+                self.send_header(header, value)
+
+
+if __name__ == '__main__':
+    SimpleHTTPServer.test(HandlerClass=GZipFriendlyRequestHandler)
